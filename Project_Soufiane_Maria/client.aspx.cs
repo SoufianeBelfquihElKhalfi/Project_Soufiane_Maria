2f5bb7635fa43d11a9d8b94fd39db57664dbe354
Soufiane <soufianebelfquih828@gmail.com>
2025-12-03 16:39:38 +0100

    lee1reserva

--- Project_Soufiane_Maria/client.aspx.cs
+++ Project_Soufiane_Maria/client.aspx.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Data.SQLite;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
@@ -11,10 +12,10 @@ namespace Project_Soufiane_Maria
         protected Label LabelDob;
         protected Label LabelAddress;
         protected Label LabelMobile;
-        protected Label LabelArrival;      
+        protected Label LabelArrival;
         protected Label LabelDeparture;
-        protected Label LabelRoom; 
-        protected Label LabelDNI;
+        protected Label LabelRoom;
+        protected Label LabelUser;
 
         protected void Page_Load(object sender, EventArgs e)
         {
@@ -26,7 +27,7 @@ namespace Project_Soufiane_Maria
                     string username = Session["username"].ToString();
                     string profile = Session["profile"].ToString();
 
-                  //  LabelWelcome.Text = "Welcome, " + profile + " " + username;
+                    LabelWelcome.Text = "Welcome, " + profile + " " + username;
 
                     if (profile != "client")
                     {
@@ -57,6 +58,10 @@ namespace Project_Soufiane_Maria
                         LabelMobile.Text = string.Empty;
                     }
 
+                    // ============================
+                    // OBTENER RESERVAS DEL CLIENTE
+                    // ============================
+                    GetReservationsByClient(clientData.Id);
                 }
                 else
                 {
@@ -65,6 +70,48 @@ namespace Project_Soufiane_Maria
                 }
             }
         }
+
+        private void GetReservationsByClient(string clientId)
+        {
+            string pathDB = Server.MapPath("~/database1.db");
+
+            using (SQLiteConnection conn = new SQLiteConnection("Data Source=" + pathDB + ";Version=3;"))
+            {
+                conn.Open();
+
+                string query = @"SELECT arrival, departure, room_id 
+                         FROM reservations 
+                         WHERE client_id = @client_id";
+
+                using (SQLiteCommand cmd = new SQLiteCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@client_id", clientId);
+
+                    using (SQLiteDataReader reader = cmd.ExecuteReader())
+                    {
+                        if (reader.HasRows)
+                        {
+                            reader.Read(); // primera reserva
+
+                            LabelArrival.Text = "Arrival: " +
+                                Convert.ToDateTime(reader["arrival"]).ToShortDateString();
+
+                            LabelDeparture.Text = "Departure: " +
+                                Convert.ToDateTime(reader["departure"]).ToShortDateString();
+
+                            LabelRoom.Text = "Room ID: " + reader["room_id"].ToString();
+                        }
+                        else
+                        {
+                            LabelArrival.Text = "No reservations found.";
+                            LabelDeparture.Text = "";
+                            LabelRoom.Text = "";
+                        }
+                    }
+                }
+            }
+        }
+
         protected void btnLogout_Click(object sender, EventArgs e)
         {
             Session.Clear();
